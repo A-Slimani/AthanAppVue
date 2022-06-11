9a0a461cd0546b92b32878d05a8f69852e8c6c63
AbdullahS98 <abdullahslimani98@gmail.com>
2022-06-11 22:56:37 +1000

    first commit

---
+++ src/components/AthanTable.vue
@@ -0,0 +1,23 @@
+<script>import axios from 'axios'
+export default {
+	data() {
+		return {
+			athanTimes: []
+		}
+	},
+	methods: {
+		async getAthanTimes() {
+			const res = await axios.get('https://api.aladhan.com/v1/timingsByCity?city=Liverpool&country=Australia')
+			this.athanTimes = await res.data.data.timings
+		}
+	},
+	mounted() {
+		this.getAthanTimes();
+	}
+}
+</script>
+
+<template>
+	<h1>Athan App</h1>
+	<ul v-for="(item, index) in athanTimes">{{ index }} : {{ item }}</ul>
+</template>
---
+++ src/views/AthanView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AthanTable from '@/components/AthanTable.vue' 
+</script>
+
+<template>
+	<main>
+		<AthanTable />
+	</main>
+</template>
